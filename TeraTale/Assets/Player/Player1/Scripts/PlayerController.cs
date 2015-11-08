6926f9c61578d94076a8bf472a404ba32063887d
Lobo-Prix <lobo_prix@naver.com>
2015-11-09 01:04:41 +0900

    PlayerHandler

--- TeraTale/Assets/Player/Player1/Scripts/PlayerController.cs
+++ TeraTale/Assets/Player/Player1/Scripts/PlayerController.cs
@@ -3,21 +3,32 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector3 _destination;
+    NavMeshAgent _navMeshAgent;
+    Animator _animator;
 
     public Vector3 destination
     {
-        get { return _destination; }
-        set { _destination = value; }
+        set { _navMeshAgent.destination = value; }
     }
 
     void Start()
     {
-
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponentInChildren<Animator>();
     }
-    
+
     void Update()
     {
+        _animator.SetFloat("Speed", _navMeshAgent.velocity.magnitude);
+    }
 
+    void Attack()
+    {
+        _animator.SetTrigger("Attacking");
+    }
+
+    void Die()
+    {
+        _animator.SetTrigger("Dying");
     }
 }
