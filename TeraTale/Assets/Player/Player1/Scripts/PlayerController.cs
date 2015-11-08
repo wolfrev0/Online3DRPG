26515617d936949df2d1f0ffe9cf23ae524631ca
Lobo-Prix <lobo_prix@naver.com>
2015-11-08 22:02:26 +0900

    PlayerController#1

---
+++ TeraTale/Assets/Player/Player1/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 _destination;
+
+    public Vector3 destination
+    {
+        get { return _destination; }
+        set { _destination = value; }
+    }
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+
+    }
+}
