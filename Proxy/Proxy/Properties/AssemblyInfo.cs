25c823caf0c96f1430b11289d508708cd8692c88
Lobo-Prix <lobo_prix@naver.com>
2015-11-12 01:03:24 +0900

    Network Sturcture Planning

--- Proxy/Proxy/Properties/AssemblyInfo.cs
+++ Proxy/Proxy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("Login")]
+[assembly: AssemblyTitle("Proxy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Login")]
+[assembly: AssemblyProduct("Proxy")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("e2d644b8-116c-4fd0-8ace-0cf8c43ed5ef")]
+[assembly: Guid("980d023b-1d4a-4045-9884-ee37c935ade3")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
