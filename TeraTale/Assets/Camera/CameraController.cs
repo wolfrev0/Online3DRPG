b6f7c8268c28b29c67cfe6eaac69ab9e3315c22a
kangseonho <ksk9812141@gmail.com>
2015-11-11 21:18:13 +0900

    Revert "CameraZoomInFuction"
    This reverts commit f0a85a913c5256894774c4c56109cf8d9e246889.

--- TeraTale/Assets/Camera/CameraController.cs
+++ TeraTale/Assets/Camera/CameraController.cs
@@ -4,27 +4,12 @@ using System.Collections;
 public class CameraController : MonoBehaviour
 {
     public Transform _target;
-    Vector3 relativeAtTargetPos = new Vector3(0, 6, -6);
-
+    
     void Update()
     {
         if (_target != null)
         {
-            transform.position = _target.transform.position + relativeAtTargetPos;
-
-            if (3 > relativeAtTargetPos.magnitude)
-            {
-                relativeAtTargetPos = relativeAtTargetPos.normalized * 3;
-            }
-            else if (11 < relativeAtTargetPos.magnitude)
-            {
-                relativeAtTargetPos = relativeAtTargetPos.normalized * 11;
-            }
-            else
-            {
-                float wheelScroll = Input.GetAxis("Mouse ScrollWheel");
-                relativeAtTargetPos += new Vector3(0, -wheelScroll, wheelScroll);
-            }
+            transform.position = _target.transform.position + new Vector3(0, 8, -8);
         }
     }
 }
