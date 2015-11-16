71ef5ee22a8313b63461b24f4ab9ff8a4d69db9d
kangseonho <ksk9812141@gmail.com>
2015-11-16 20:54:20 +0900

    Costomizing and Trail
    2015-11-16
    
    Add CostomizingScene
    Add CostomizingScene Folder
    Add ParsingDataScript
    Add ReadDataScript
    Add SnowMan(Town,Costomizing)
    Add Player1 TrailRenderer
    Add TextureInfo.txt

--- TeraTale/Assets/Camera/CameraController.cs
+++ TeraTale/Assets/Camera/CameraController.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.Serialization;
 
 public class CameraController : MonoBehaviour
 {
     public Transform _target;
+    float scrollSpeed = 100.0f;
     Vector3 relativeAtTargetPos = new Vector3(0, 6, -6);
 
     void Update()
@@ -12,6 +14,7 @@ public class CameraController : MonoBehaviour
         {
             transform.position = _target.transform.position + relativeAtTargetPos;
 
+            Vector3 pos = transform.position;
             if (3 > relativeAtTargetPos.magnitude)
             {
                 relativeAtTargetPos = relativeAtTargetPos.normalized * 3;
@@ -22,7 +25,7 @@ public class CameraController : MonoBehaviour
             }
             else
             {
-                float wheelScroll = Input.GetAxis("Mouse ScrollWheel");
+                float wheelScroll = Input.GetAxis("Mouse ScrollWheel"); 
                 relativeAtTargetPos += new Vector3(0, -wheelScroll, wheelScroll);
             }
         }
---
+++ TeraTale/Assets/CostomizingScene/ParsingData.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+public class ParsingData : MonoBehaviour
+{ 
+    public Texture[] HeadTexture;
+    public Texture[] BodyTexture;
+
+    MeshRenderer mesh;
+    int _curindex = 0;
+    int __curindex = 0;
+
+	void Start ()
+    {
+        mesh = GetComponent<MeshRenderer>();    
+	}
+	
+	void Update ()
+    {
+        if (_curindex <= 0)
+            _curindex = 0;
+        else if (_curindex >= HeadTexture.Length)
+            _curindex = HeadTexture.Length - 1;
+
+        if (__curindex <= 0)
+            __curindex = 0;
+        else if (__curindex >= BodyTexture.Length)
+            __curindex = BodyTexture.Length - 1;
+
+        mesh.materials[0].mainTexture = HeadTexture[_curindex];
+        mesh.materials[1].mainTexture = BodyTexture[__curindex];
+    }
+
+    public void PreHead()
+    {
+        _curindex--;
+        Debug.Log(_curindex);
+    }
+
+    public void NextHead()
+    {
+        _curindex++;
+        Debug.Log(_curindex);
+    }
+
+    public void PreBody()
+    {
+        __curindex--;
+    }
+
+    public void NextBody()
+    {
+        __curindex++;
+    }
+
+    public void SaveData()
+    {
+        StreamWriter sw = new StreamWriter(new FileStream("D:/Desktop/Projects/TeraTale/TextureInfo.txt", FileMode.Create));
+
+        sw.WriteLine(_curindex);
+
+        sw.WriteLine(__curindex);
+
+        sw.Close();
+    }
+    public void LoadData()
+    {
+        Application.LoadLevel(4);
+    }
+}
