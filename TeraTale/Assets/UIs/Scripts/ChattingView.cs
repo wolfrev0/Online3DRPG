814668e20cc9cc20f9cbb96bcb70ec18d7b797a1
Lobo-Prix <lobo_prix@naver.com>
2015-12-27 16:40:06 +0900

    Local Chatting UI
    [todo]
    채팅 내용에 채팅유저 이름 표시하기
    RPC로 네트워크 송신

---
+++ TeraTale/Assets/UIs/Scripts/ChattingView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChattingView : MonoBehaviour
+{
+    Text _text;
+
+	void Start ()
+    {
+        _text = GetComponent<Text>();
+	}
+
+    public void PushChat(string chat)
+    {
+        if (LayoutUtility.GetPreferredHeight(_text.rectTransform) > _text.rectTransform.rect.height)
+        {
+            _text.text = _text.text.Remove(0, _text.text.IndexOf('\n') + 1);
+        }
+        _text.text += chat += "\n";
+    }
+}
