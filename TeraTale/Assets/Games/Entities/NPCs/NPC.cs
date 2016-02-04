e888f4faf9e82ca377a326bc6d093d0a4d5ee50d
kangseonho <ksk9812141@gmail.com>
2016-02-05 00:27:06 +0900

    npc move and about

--- TeraTale/Assets/Games/Entities/NPCs/NPC.cs
+++ TeraTale/Assets/Games/Entities/NPCs/NPC.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public abstract class NPC : Entity
 {
     public struct Script
@@ -15,48 +16,58 @@ public abstract class NPC : Entity
         public string comment;
         public List<Command> commands;
     }
+    public Transform[] _trans;
     public GameObject speechBubble;
+    float _fDelay;
     Camera npcCam;
     NPCDialog npcDialog;
     List<Script> _scripts = new List<Script>();
+    NavMeshAgent _nav;
+    Animator _animator;
+    bool bChk;
 
     protected void Awake()
     {
         npcCam = GameObject.FindWithTag("NPCCamera").GetComponent<Camera>();
         npcDialog = FindObjectOfType<NPCDialog>();
+        _nav = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        _fDelay = Time.time; 
 
         Script s;
         Script.Command cmd;
 
-        s.commands = new List<Script.Command>();
-        s.comment = "안녕";
-        cmd.name = "Next";
-        cmd.action = npcDialog.Next;
-        s.commands.Add(cmd);
-        cmd.name = "Close";
-        cmd.action = npcDialog.Close;
-        s.commands.Add(cmd);
-        _scripts.Add(s);
+        //s.commands = new List<Script.Command>();
+        //s.comment = "안녕";
+        //cmd.name = "Next";
+        //cmd.action = npcDialog.Next;
+        //s.commands.Add(cmd);
+        //cmd.name = "Close";
+        //cmd.action = npcDialog.Close;
+        //s.commands.Add(cmd);
+        //_scripts.Add(s);
 
-        s.commands = new List<Script.Command>();
-        s.comment = "ㅎㅎㅎ";
-        cmd.name = "Next";
-        cmd.action = npcDialog.Next;
-        s.commands.Add(cmd);
-        cmd.name = "Close";
-        cmd.action = npcDialog.Close;
-        s.commands.Add(cmd);
-        _scripts.Add(s);
+        //s.commands = new List<Script.Command>();
+        //s.comment = "ㅎㅎㅎ";
+        //cmd.name = "Next";
+        //cmd.action = npcDialog.Next;
+        //s.commands.Add(cmd);
+        //cmd.name = "Close";
+        //cmd.action = npcDialog.Close;
+        //s.commands.Add(cmd);
+        //_scripts.Add(s);
 
-        s.commands = new List<Script.Command>();
-        s.comment = "잘가";
-        cmd.name = "Next";
-        cmd.action = npcDialog.Next;
-        s.commands.Add(cmd);
-        cmd.name = "Close";
-        cmd.action = npcDialog.Close;
-        s.commands.Add(cmd);
-        _scripts.Add(s);
+        //s.commands = new List<Script.Command>();
+        //s.comment = "잘가";
+        //cmd.name = "Next";
+        //cmd.action = npcDialog.Next;
+        //s.commands.Add(cmd);
+        //cmd.name = "Close";
+        //cmd.action = npcDialog.Close;
+        //s.commands.Add(cmd);
+        //_scripts.Add(s);
+
+        Invoke("Around", 2);
     }
 
     public void StartConversation()
@@ -70,5 +81,35 @@ public abstract class NPC : Entity
         npcCam.depth = Camera.main.depth + 1;
 
         npcDialog.StartConversation(_scripts);
+    }
+
+    void Around()
+    {
+        if (_nav.velocity.magnitude == 0f && bChk)
+        {   
+            _nav.SetDestination(_trans[0].position);
+
+            bChk = !bChk;
+        }
+        else if(_nav.velocity.magnitude == 0f && !bChk)
+        {
+            _nav.SetDestination(_trans[1].position);
+
+            bChk = !bChk;
+        }
+
+        _animator.SetBool("Walk", true);
+        Invoke("Return", UnityEngine.Random.Range(3, 5));
+
+        Debug.Log(bChk.ToString());
+    }
+
+    void Return()
+    {
+        _animator.SetBool("Walk", false);
+
+        transform.localEulerAngles = new Vector3(0, 0, 0);
+
+        Invoke("Around", UnityEngine.Random.Range(3, 5));
     }
 }
