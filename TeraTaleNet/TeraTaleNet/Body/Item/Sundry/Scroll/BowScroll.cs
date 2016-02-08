32509b62e8fbe06864d7e2a529d3a5782291b482
Lobo-Prix <lobo_prix@naver.com>
2016-02-08 19:13:22 +0900

    New NPC Smith

---
+++ TeraTaleNet/TeraTaleNet/Body/Item/Sundry/Scroll/BowScroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace TeraTaleNet
+{
+    public class BowScroll : Scroll
+    {
+        public sealed override int price { get { return 20; } }
+        
+        public override List<IngradientItem> ingredients
+        {
+            get
+            {
+                var ret = new List<IngradientItem>();
+                ret.Add(new IngradientItem("Log", 10));
+                ret.Add(new IngradientItem("IronOre", 3));
+                ret.Add(new IngradientItem("Bone", 1));
+                return ret;
+            }
+        }
+
+        public BowScroll()
+        { }
+    }
+}
---
+++ TeraTaleNet/TeraTaleNet/Body/Item/Sundry/Scroll/Scroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace TeraTaleNet
+{
+    public abstract class Scroll : Sundry
+    {
+        public class IngradientItem
+        {
+            string itemName;
+            int count;
+
+            public IngradientItem(string itemName, int count)
+            {
+                this.itemName = itemName;
+                this.count = count;
+            }
+        }
+
+        public sealed override string effectExplanation { get { return ""; } }
+        public sealed override string explanation { get { return "조합 재료가 빼곡히 적혀있다."; } }
+        public abstract List<IngradientItem> ingredients { get; }
+
+        public Scroll()
+        { }
+    }
+}
