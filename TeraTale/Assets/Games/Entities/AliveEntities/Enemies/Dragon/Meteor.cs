e5607d98e4379ec8efa9e40ca79da09a3bc03c0e
Lobo-Prix <lobo_prix@naver.com>
2016-02-10 14:39:27 +0900

    Dragon Decorate

--- TeraTale/Assets/Games/Entities/AliveEntities/Enemies/Dragon/Meteor.cs
+++ TeraTale/Assets/Games/Entities/AliveEntities/Enemies/Dragon/Meteor.cs
@@ -3,9 +3,13 @@
 public class Meteor : MonoBehaviour
 {
     public GameObject pfMagicCircle;
+    public GameObject pfExplosion;
 
     Projectile _projectile;
     GameObject _magicCircle;
+    MeshRenderer _magicCircleRenderer;
+    Vector3 _startPos;
+    Vector3 _endPos;
 
     void Awake()
     {
@@ -18,10 +22,21 @@ public class Meteor : MonoBehaviour
         Physics.Raycast(transform.position, _projectile.direction, out hit, float.MaxValue, LayerMask.GetMask("Terrain"));
         _magicCircle = Instantiate(pfMagicCircle);
         _magicCircle.transform.position = hit.point;
+        _magicCircleRenderer = _magicCircle.GetComponentInChildren<MeshRenderer>();
+        _startPos = transform.position;
+        _endPos = hit.point;
+    }
+
+    void Update()
+    {
+        _magicCircleRenderer.material.color = Color.Lerp(Color.red, Color.yellow, (_endPos - transform.position).magnitude / (_endPos - _startPos).magnitude);
     }
 
     void OnDestroy()
     {
         Destroy(_magicCircle);
+        var fxExplosion = Instantiate(pfExplosion);
+        fxExplosion.transform.position = transform.position;
+        Destroy(fxExplosion, 2);
     }
 }
