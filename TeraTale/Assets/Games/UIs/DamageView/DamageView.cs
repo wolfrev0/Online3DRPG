e118023bc3d0ceac038500d6d044574f5b101e83
Lobo-Prix <lobo_prix@naver.com>
2016-02-12 01:40:21 +0900

    DamageView

---
+++ TeraTale/Assets/Games/UIs/DamageView/DamageView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageView : MonoBehaviour
+{
+    public float life = 1;
+    public float speed = 1;
+    Text _text;
+
+    void Awake()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    void Start()
+    {
+        Destroy(gameObject, life);
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+    }
+
+    public void SetDamage(float amount)
+    {
+        _text.text = amount.ToString();
+    }
+}
