ecb08df199a26e93740e266208c6181bf663c3b9
Lobo-Prix <lobo_prix@naver.com>
2016-02-12 19:00:52 +0900

    Dragon Attack Syncing

--- TeraTale/Assets/Games/Entities/AliveEntities/Enemies/Dragon/Scripts/Dragon.cs
+++ TeraTale/Assets/Games/Entities/AliveEntities/Enemies/Dragon/Scripts/Dragon.cs
@@ -8,10 +8,21 @@ public class Dragon : Enemy
     public FireBreath fireBreath;
 
     Collider _ardColl;
+    int nextAttackType;
+    System.Random random = new System.Random();
+
     public override void OnAttackAnimationEnd(Collider ardColl)
     {
         _ardColl = ardColl;
-        Invoke("ARDCollEnable", Random.Range(2, 8));
+        if (isServer)
+            Send(new SetDragonNextAttack(Random.Range(2f, 8f), Random.Range(0, 2), Random.Range(int.MinValue, int.MaxValue)));
+    }
+
+    public void SetDragonNextAttack(SetDragonNextAttack rpc)
+    {
+        Invoke("ARDCollEnable", rpc.nextEnableDelay);
+        nextAttackType = rpc.attackType;
+        random = new System.Random(rpc.seed);
     }
 
     void ARDCollEnable()
@@ -22,7 +33,7 @@ public class Dragon : Enemy
     public override void Attack()
     {
         base.Attack();
-        _animator.SetInteger("AttackType", Random.Range(0, 2));
+        _animator.SetInteger("AttackType", nextAttackType);
     }
 
     new void Awake()
@@ -47,15 +58,15 @@ public class Dragon : Enemy
 
     void MeteorStart()
     {
-        for (int i = 0; i < 8; i++)
-            Invoke("CastMeteor", Random.Range(0f, 2f));
+        for (int i = 0; i < 12; i++)
+            Invoke("CastMeteor", (float)random.NextDouble());
     }
 
     void CastMeteor()
     {
         var meteor = Instantiate(pfMeteor);
-        var xzSeed = Random.Range(0, Mathf.PI * 2);
-        meteor.transform.position = transform.position + new Vector3(Mathf.Sin(xzSeed), 0, Mathf.Cos(xzSeed)) * Random.Range(0f, 10f) + Vector3.up * 50;
+        var xzSeed = (float)random.NextDouble() * 2 * Mathf.PI;
+        meteor.transform.position = transform.position + new Vector3(Mathf.Sin(xzSeed), 0, Mathf.Cos(xzSeed)) * (float)random.NextDouble() * 10 + Vector3.up * 50;
         meteor.direction = new Vector3(0, -1, 0);
     }
 
---
+++ TeraTaleNet/TeraTaleNet/Body/RPC/SetNextAttack.cs
@@ -0,0 +1,21 @@
+﻿namespace TeraTaleNet
+{
+    public class SetDragonNextAttack : RPC
+    {
+        public float nextEnableDelay;
+        public int attackType;
+        public int seed;
+
+        public SetDragonNextAttack(float nextEnableDelay, int attackType, int seed)
+            : base(RPCType.All)
+        {
+            this.nextEnableDelay = nextEnableDelay;
+            this.attackType = attackType;
+            this.seed = seed;
+        }
+
+        public SetDragonNextAttack()
+            : base(RPCType.All)
+        { }
+    }
+}
