a0277d719cf99cd7be6831fa9ef684174e1b9730
Lobo-Prix <lobo_prix@naver.com>
2016-02-15 22:06:34 +0900

    New Cursor

---
+++ TeraTale/Assets/Games/UIs/PlayerWeaponSlot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerWeaponSlot : MonoBehaviour
+{
+    Image _image;
+
+    void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    void OnEnable()
+    {
+        _image.sprite = Player.mine.weapon.sprite;
+    }
+}
