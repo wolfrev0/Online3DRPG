89092ae7f6af99665faea4bf50267962a7fd68ae
Lobo-Prix <lobo_prix@naver.com>
2016-02-16 13:47:25 +0900

    HealView & FakeLoading & Sound

---
+++ TeraTale/Assets/Games/UIs/FakeLoading/FakeLoading.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FakeLoading : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public Image loadingBar;
+    public Text loadingText;
+    Image image;
+    Vector2 _destination;
+    float _elapsed = 0;
+    const float loadingDelay = 1;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+        image.sprite = sprites[Random.Range(0, sprites.Length)];
+        Invoke("Close", loadingDelay);
+    }
+
+    void Update()
+    {
+        _elapsed += Time.deltaTime;
+        image.rectTransform.anchoredPosition *= 9;
+        image.rectTransform.anchoredPosition += _destination;
+        image.rectTransform.anchoredPosition /= 10;
+        loadingBar.fillAmount = _elapsed / loadingDelay;
+        loadingText.text = string.Format("{0:0}%", _elapsed / loadingDelay * 100); ;
+    }
+
+    void Close()
+    {
+        _destination = Vector2.up * 900;
+        Invoke("Remove", 2);
+    }
+
+    void Remove()
+    {
+        Destroy(gameObject);
+    }
+}
