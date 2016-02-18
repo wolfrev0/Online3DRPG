68d4cbe629a093aa0318586cb408f77759c66cf7
Lobo-Prix <lobo_prix@naver.com>
2016-02-18 10:52:37 +0900

    LapisLazuliOre

---
+++ TeraTaleNet/TeraTaleNet/Body/Item/Sundry/LapisLazuli.cs
@@ -0,0 +1,12 @@
+﻿namespace TeraTaleNet
+{
+    public class LapisLazuliOre : Sundry
+    {
+        public sealed override int price { get { return 60; } }
+        public sealed override string effectExplanation { get { return ""; } }
+        public sealed override string explanation { get { return "청금석. 매우 희귀해보인다."; } }
+
+        public LapisLazuliOre()
+        { }
+    }
+}
