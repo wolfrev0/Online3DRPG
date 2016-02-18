37d61eb4d53fe7d77fa4dd6ea7a6bdb9be4f450c
kangseonho <ksk9812141@gmail.com>
2016-02-18 22:30:30 +0900

    UI Quit and Credit

---
+++ TeraTale/Assets/Games/UIs/Credit/CreditDialog.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditDialog : MonoBehaviour
+{
+    public GameObject _credit = null;
+
+    public void OnCredit()
+    {
+        _credit.SetActive(true);
+    }
+
+    public void OffCredit()
+    {
+        _credit.SetActive(false);
+    }
+}
---
+++ TeraTale/Assets/Games/UIs/Quit/QuitDialog.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitDialog : MonoBehaviour
+{
+    public GameObject _quitPopup;
+
+   public void ShowQuit()
+    {
+        _quitPopup.SetActive(true);
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    public void OffQuit()
+    {
+        _quitPopup.SetActive(false);
+    }
+}
