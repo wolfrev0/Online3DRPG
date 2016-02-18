674085b0e0621c01d486a008b78571260139baf2
Lobo-Prix <lobo_prix@naver.com>
2016-02-18 21:34:28 +0900

    Skill

---
+++ TeraTale/Assets/PlayerSkill.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerSkill : StateMachineBehaviour
+{
+    Player _player;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (_player == null)
+            _player = animator.GetComponent<Player>();
+        InputHandler.instance.enabled = false;
+    }
+
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _player.SetKnockdown(false);
+        _player.StopAttack();
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
