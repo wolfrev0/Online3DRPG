33f41a4f5ed5fdd1d1a69d8ef74c94742552db68
Lobo-Prix <lobo_prix@naver.com>
2016-02-20 23:55:08 +0900

    Quit

---
+++ TeraTale/Assets/Games/UIs/Quit/QuitDialog.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitDialog : MonoBehaviour
+{
+    public GameObject _quitPopup;
+
+   public void ShowQuit()
+    {
+        _quitPopup.SetActive(true);
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+
+    public void OffQuit()
+    {
+        _quitPopup.SetActive(false);
+    }
+}
